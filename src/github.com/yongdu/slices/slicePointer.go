2c2b06ae010f111ce931a2fbb6687d52f8d54d6c
Ubuntu <ubuntu@ip-172-31-32-194.us-east-2.compute.internal>
2018-02-27 22:10:01 +0000

    index value of slices, map

--- src/github.com/yongdu/slices/slicePointer.go
+++ src/github.com/yongdu/slices/slicePointer.go
@@ -11,6 +11,24 @@ func main(){
 
         fmt.Println(slice)
         fmt.Println(newslice,newslice2)
+        for index, value := range slice {
+            fmt.Printf("Index: %d Value: %d\n", index, value)
+        }
+        for index, value := range slice{
+            // the address of value is same after each iterate 
+            fmt.Printf("Value: %d Value-Addr: %X ElementAddress: %X \n",value,&value,&slice[index])
+        }
+        dict := map[string]string{"Red":"#da117","Orange":"#e95a22"}
+        // get value based on the key
+        value,exists := dict["Blue"]
 
+        if exists {
+            fmt.Println(value)
+        }
+
+        value1:= dict["Red"]
+        if value1!= ""{
+            fmt.Println(value1)
+        }
 }
 
